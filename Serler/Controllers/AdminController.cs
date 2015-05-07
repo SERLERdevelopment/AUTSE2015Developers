09f32c1934d5d6b8a48093976ea43352ff00898c
Alyssa Byun <alyssa.byun@hotmail.com>
2015-05-08 00:08:22 +1200

    created all users list

--- Serler/Controllers/AdminController.cs
+++ Serler/Controllers/AdminController.cs
@@ -20,14 +20,99 @@ namespace Serler.Controllers
             return View();
         }
 
-        public ActionResult AllUsers()
+        [HttpGet]
+        public ActionResult ModifyUser(int id)
         {
             using (var conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Serler"].ConnectionString))
             {
+                var query = "select * from Users where UserId = @UserId";
+                conn.Open();
+                var model = conn.Query<UserViewModel>(query, new { UserId = id }).FirstOrDefault();
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ModifyUser(UserViewModel model)
+        {
+            return View(model);
+        }
+
+        public ActionResult AllUsers(string searchText, int? page)
+        {
+            if (page == null || page < 0)
+            {
+                page = 1;
+            }
+
+            if (string.IsNullOrEmpty(searchText))
+            {
+                searchText = "";
+            }
+            var model = new PagedViewModel<UserViewModel>();
+
+            using (var conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Serler"].ConnectionString))
+            {
+                var countQuery = "select COUNT(UserId) from Users where Email like @searchText;";
                 var query = string.Format("select * from Users where IsActive = 1 order by Email;");
                 conn.Open();
+                model.TotalCount = conn.Query<int>(countQuery, new { searchText = "%" + searchText + "%" }).FirstOrDefault();
+                model.SearchModel = new SearchModel
+                {
+                    SearchText = "%" + searchText + "%",
+                    Take = 10
+                };
+                model.SearchModel.Skip = (page.Value - 1) * model.SearchModel.Take;
+
                 var member = conn.Query<UserViewModel>(query).ToList();
-                return View(member);
+                if(member != null)
+                {
+                    model.Result = member;
+                }
+                var maxPage = Math.Ceiling((double)model.TotalCount / model.SearchModel.Take);
+
+                var pagination1 = new PaginationModel();
+                pagination1.DisplayLabel = "<<";
+                pagination1.Url = "~/AllUsers?page=1";
+                pagination1.IsEnable = page <= 1 ? false : true;
+
+                var pagination2 = new PaginationModel();
+                pagination2.DisplayLabel = "<";
+                pagination2.Url = "~/AllUsers?page=" + (page - 1);
+                pagination2.IsEnable = page <= 1 ? false : true;
+
+                var pagination3 = new PaginationModel();
+                pagination3.DisplayLabel = ">";
+                pagination3.Url = "~/AllUsers?page=" + (page + 1);
+                pagination3.IsEnable = page >= maxPage ? false : true;
+
+                var pagination4 = new PaginationModel();
+                pagination4.DisplayLabel = ">>";
+                pagination4.Url = "~/AllUsers?page=" + (page - 1);
+                pagination4.IsEnable = page >= maxPage ? false : true;
+
+                model.PaginationModel = new List<PaginationModel>();
+                model.PaginationModel.Add(pagination1);
+                model.PaginationModel.Add(pagination2);
+
+                for (int i = 0; i < maxPage; i++)
+                {
+                    var paginationModel = new PaginationModel();
+                    paginationModel.DisplayLabel = i + 1 + "";
+                    paginationModel.Url = "~/AllUsers?page=" + (i + 1);
+                    paginationModel.IsEnable = true;
+                    if (i + 1 == page)
+                    {
+                        paginationModel.IsActive = true;
+                    }
+                    model.PaginationModel.Add(paginationModel);
+                }
+
+                model.PaginationModel.Add(pagination3);
+                model.PaginationModel.Add(pagination4);
+
+                return View(model);
             }
         }
 
---
+++ Serler/Models/PagedViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Serler.Models
+{
+    public class PagedViewModel<T>
+    {
+        public IEnumerable<T> Result { get; set; }
+        public int TotalCount { get; set; }
+        public int MyProperty { get; set; }
+        public SearchModel SearchModel { get; set; }
+        public List<PaginationModel> PaginationModel { get; set; }
+    }
+
+    public class SearchModel
+    {
+        public int Skip { get; set; }
+        public int Take { get; set; }
+        public string SearchText { get; set; }
+    }
+
+    public class PaginationModel
+    {
+        public string DisplayLabel { get; set; }
+        public string Url { get; set; }
+        public bool IsEnable { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
