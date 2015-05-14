6599ded97164c0efd661e667b4d7012c105aa001
Alyssa Byun <alyssa.byun@hotmail.com>
2015-05-14 23:22:28 +1200

    added activity log controller

---
+++ Serler/Controllers/ActivityLogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Serler.Controllers
+{
+    public class ActivityLogController : Controller
+    {
+        //
+        // GET: /ActivityLog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
