dc6cfd1ee7248ff8b9ee8946093fbf4520ba7df4
romangn <gubaidullin.roman@gmail.com>
2015-06-01 22:46:03 +1200

    Change to PDF framework;
    Plus deletion of unnecessary references and creation of quick
    submission-bugged for now.

--- Serler/Controllers/PaperController.cs
+++ Serler/Controllers/PaperController.cs
@@ -6,11 +6,8 @@ using System.Web.Mvc;
 using Serler.Models;
 using System.Data.SqlClient;
 using Dapper;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
-using iTextSharp.text.html.simpleparser;
-using iTextSharp.tool.xml;
 using System.IO;
+using Rotativa.Options;
 
 namespace Serler.Controllers
 {
@@ -253,31 +250,7 @@ namespace Serler.Controllers
                 switch (submit)
                 {
                     case "Save":
-                        Byte[] bytes;
-                        using (var ms = new MemoryStream())
-                        {
-                            using (var doc = new Document())
-                            {
-                                using (var writer = PdfWriter.GetInstance(doc, ms))
-                                {
-                                    doc.Open();
-                                    var my_html = @"<p>This <em>is </em><span class=""headline"" style=""text-decoration: underline;"">some</span> <strong>sample <em> text</em></strong><span style=""color: red;"">!!!</span></p>";
-                                    var my_css = @".headline{font-size:100%}";
-                                    using (var msCss = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(my_css)))
-                                    {
-                                        using (var msHtml = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(my_html)))
-                                        {
-                                            iTextSharp.tool.xml.XMLWorkerHelper.GetInstance().ParseXHtml(writer, doc, msHtml, msCss);
-                                        }
-                                    }
-                                    doc.Close();
-                                }
-                            }
-                            bytes = ms.ToArray();
-                        }
-                        var testFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "test.pdf");
-                        System.IO.File.WriteAllBytes(testFile, bytes);
-                        return RedirectToAction("ViewPapers");
+                        return new Rotativa.ViewAsPdf("ViewPaper", model);
                         break;
                 }
             }
